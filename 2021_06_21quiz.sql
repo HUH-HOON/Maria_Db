b79b0ca11260ebd39471b7de3376759f7d87e729
HUH-HOON <gjgns1994@naver.com>
2021-07-06 19:37:56 +0900

    no message

---
+++ 2021_06_21quiz.sql
@@ -0,0 +1,72 @@
+Use employees ; 
+-- 1.  모든 부서의 이름(departments.dept_name)과  그 부서에 근무하는 모든 사원의 이름(employees.first_name 과 employees.last_name )을 출력하세요.   
+select departments.dept_name, employees.first_name, employees.last_name
+FROM departments INNER JOIN dept_emp
+on departments.dept_no = dept_emp.dept_no
+JOIN employees
+on employees.emp_no = dept_emp.emp_no
+where dept_emp.to_date = '9999-01-01';
+
+
+-- 2.  d007번 부서의 이름(departments.dept_name)과  그 부서에 근무하는 모든 사원의 이름(employees.first_name  과 employees.last_name )을 출력하세요.
+select departments.dept_name, employees.first_name, employees.last_name, departments.dept_no
+FROM departments INNER JOIN dept_emp
+on departments.dept_no = dept_emp.dept_no
+JOIN employees
+on employees.emp_no = dept_emp.emp_no
+WHERE departments.dept_no = 'd007'
+AND dept_emp.to_date = '9999-01-01';
+
+
+-- 3.  d007번 부서의 이름(departments.dept_name)과 그 부서에 근무하는 모든 사원 중 남자인 사원의 모든 사원의 이름(employees.first_name  과 employees.last_name )을 출력하세요.
+select departments.dept_name, employees.first_name, employees.last_name, departments.dept_no
+FROM departments INNER JOIN dept_emp
+on departments.dept_no = dept_emp.dept_no
+JOIN employees
+on employees.emp_no = dept_emp.emp_no
+WHERE departments.dept_no = 'd007'
+AND employees.gender = 'M'
+AND dept_emp.to_date = '9999-01-01';
+
+
+-- 4.  d007번 부서의 이름(departments.dept_name)과 그 부서에 근무하는 모든 사원 중 남자이고, 2000년에 입사한모든 사원의 이름(employees.first_name  과 employees.last_name )을 출력하세요.
+select departments.dept_name, employees.first_name, employees.last_name, departments.dept_no
+FROM departments INNER JOIN dept_emp
+on departments.dept_no = dept_emp.dept_no
+JOIN employees
+on employees.emp_no = dept_emp.emp_no
+WHERE departments.dept_no = 'd007'
+AND employees.gender = 'M';
+
+
+-- 5.  d007번 부서의 이름(departments.dept_name)과 그 부서에 근무하는 모든 사원 중 남자이고, 2000년에 입사한모든 사원의 이름(employees.first_name  과 employees.last_name )을 출력하세요. 
+-- 단, 사원이름(employees.first_name) 오름차순으로 정렬하여 출력하세요 .
+select departments.dept_name, employees.first_name, employees.last_name, departments.dept_no
+FROM departments INNER JOIN dept_emp
+on departments.dept_no = dept_emp.dept_no
+JOIN employees
+on employees.emp_no = dept_emp.emp_no
+WHERE employees.hire_date = '2000*'
+AND departments.dept_no = 'd007'
+AND employees.gender = 'M'
+order by employees.first_name desc;
+ 
+-- 6,   Staff 업무(titles.title) 로 근무한 적이 있는 모든 사원의 이름(employees.first_name  과 employees.last_name )을 출력하세요. 
+SELECT titles.title, employees.first_name, employees.last_name
+FROM titles INNER JOIN employees
+on titles.emp_no = employees.emp_no;
+  
+-- 7.   Staff 업무(titles.title) 로 근무한 적이 있는 모든 사원의 평균 급여(salaries.salary) 를 출력하세요. 
+SELECT titles.title, AVG(salaries.salary) AS Average 
+FROM titles INNER JOIN salaries
+on titles.emp_no = salaries.emp_no 
+where titles.title = 'Staff';
+ 
+-- 8.  업무(titles.title) 별로  사원들의 평균 급여(salaries.salary) 를 출력하세요.
+SELECT titles.title, avg(salaries.salary)
+FROM titles INNER JOIN salaries
+on titles.emp_no = salaries.emp_no
+GROUP BY titles.title;
+ 
+
+
---
+++ Join.sql
@@ -0,0 +1,64 @@
+
+-- inner join
+-- 모든 사용자에 대한 원하는 조건 불러오기.
+SELECT usertbl.userID, usertbl.name, buytbl.prodName, buytbl.price
+FROM usertbl INNER JOIN buytbl
+ON usertbl.userID=buytbl.userID;
+
+-- 서울 사용자에 대한 원하는 조건  불러오기.
+
+SELECT usertbl.userID, usertbl.name, buytbl.prodName, buytbl.price, usertbl.addr
+FROM usertbl INNER JOIN buytbl
+ON usertbl.userID=buytbl.userID
+WHERE usertbl.addr = '서울'
+AND prodname = '운동화';
+
+-- 윤종신이라는 사용자가 구맿나 제품명
+-- ANSI 표준
+SELECT usertbl.userID, usertbl.name, buytbl.prodName
+FROM usertbl INNER JOIN buytbl
+ON usertbl.userID=buytbl.userID
+WHERE usertbl.name = '성시경';
+
+-- ANSI 표준 (OUTER 조인)
+SELECT usertbl.userID, usertbl.name, buytbl.prodName
+FROM usertbl left OUTER JOIN buytbl
+ON usertbl.userID=buytbl.userID;
+
+SELECT usertbl.userID, usertbl.name, buytbl.prodName
+FROM usertbl right OUTER JOIN buytbl
+ON usertbl.userID=buytbl.userID;
+
+-- inner join 3개 이상의 테이블 조인
+SELECT ...
+from a join b
+on a.id = b.id
+join c
+on c.id = a.id
+
+
+-- 1) 사용자 이름, 구매제품명, 가격, 수량
+
+SELECT usertbl.name, buytbl.prodName, buytbl.price, buytbl.amount
+FROM usertbl INNER JOIN buytbl
+ON usertbl.userID=buytbl.userID;
+
+-- 2) 사용자 중 총구매액이 가장 많은 사용자 이름.
+SELECT sum(buytbl.price* buytbl.amount) 총액
+				,usertbl.userID
+FROM usertbl INNER JOIN buytbl
+ON usertbl.userID=buytbl.userID
+GROUP by usertbl.userID 
+order by 총액 desc
+limit 1;
+
+
+-- 별칭 주기.
+SELECT sum(b.price* b.amount) 총액
+				,u.userID
+FROM usertbl u INNER JOIN buytbl b
+ON u.userID=b.userID
+GROUP by u.userID 
+order by 총액 desc
+limit 1;
+
